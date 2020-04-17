11d3a614324e61808b85ed243a06f9d173122cea
bambidorothy <bambideleon@gmail.com>
2020-04-17 20:18:52 +0800

    added js constructor func example

--- main.js
+++ main.js
@@ -1,1 +1,9 @@
+//Constructor function example
+function Circle(radius) {//creates a circle obj
+    this.radius = radius; //creates a radius property
+    this.draw = function () { //creates a draw method
+        console.log('draw');
+    }
+}
 
+const circ = new Circle(1); //creates an instantiation of circle obj
