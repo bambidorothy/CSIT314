a286d552182c00cda0062dd22b8e767e109b1a45
bambidorothy <bambideleon@gmail.com>
2020-04-18 19:24:57 +0800

    created a Person class for testing, test obj can output on html

--- main.js
+++ main.js
@@ -1,9 +1,15 @@
-class Person {
-    constructor(fullName, userid, password, role) {
+class Person { //create Person class
+    constructor(fullName, userid, password, role) { //create constructor fields
         this.fullName = fullName;
         this.userid = userid;
         this.password = password;
         this.role = role;
     }
+    display() { //display name function
+        return "Welcome " + this.fullName + "!" + "<br>" + "Your userid is " + this.userid + "!"+ "<br>" + "Your role is " + this.role + "!";
+    }
 }
-const user = new Person(Dorothy,s001,123456,student);
+let user = new Person('Dorothy', 's001', 123456, 'student'); //create user object of person class
+
+document.getElementById("demo").innerHTML = user.display();
+console.log("Welcome " + this.fullName + "!" + "Your userid is " + this.userid + "!"+ "Your role is " + this.role + "!");
