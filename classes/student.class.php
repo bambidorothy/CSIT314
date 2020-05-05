ad653e045b5d9057acedb66dafbb3029a1d79c8c
bambidorothy <bambideleon@gmail.com>
2020-05-05 22:41:44 +0800

    multiple changes

--- classes/student.class.php
+++ classes/student.class.php
@@ -3,6 +3,7 @@ include_once 'user.class.php'; //import /classes/user.class.php
 include_once 'db_config.php';
 class Student extends User
 { //create Student class
+    public $post_id;
 
     //display a list of all Posts
     public function displayAllPosts($id)
@@ -56,8 +57,8 @@ class Student extends User
                   <td>'.$date.'</td> 
                   <td>'.$time.'</td>
                   <td>'.$status.'</td>
-                  <td><a onclick="closePost();" class="btn btn-danger" style="width:10em;">Mark as Closed</a></td>
-                  <td><a type="submit" class="btn btn-success" style="width:7em;">View Post</a></td>
+                  <td><a href="closePost.php?post_id=<?php echo $row["id"];?>" class="btn btn-danger" style="width:10em;">Mark as Closed</a></td>
+                  <td><a href="detailPost.php" class="btn btn-success" style="width:7em;">View Post</a></td>
               </tr>';
             }
             //$result->free();
@@ -67,16 +68,16 @@ class Student extends User
     }
     //mark post as closed
     public function markPostClose($post_id)
-    {
+    {   $post_id = $post_id;
         $sql="UPDATE POST SET status= 0  WHERE id= '$post_id'";
-        //echo $sql;
+        echo $sql;
         $result=mysqli_query($this->db, $sql);
-        if ($result === true) {
+/*         if ($result === true) {
             $message = "Post closed successfully!";
             echo "<script type='text/javascript'>alert('$message');</script>"; //do javascript alert upon successful suspension
             echo "<script>window.open('student.php', '_self');</script>"; //redirect back to student.php
         } else {
             echo "Error updating record: " . $this->db->error;
-        }
+        } */
     }
 }
