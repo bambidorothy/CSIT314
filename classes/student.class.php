b15dfbb082212ec8cdb5b6bb2c354f309f247aca
bunnyH0p <vash.peng@yahoo.com.sg>
2020-05-05 22:41:05 +0800

    createQuestion

--- classes/student.class.php
+++ classes/student.class.php
@@ -1,7 +1,22 @@
 <?php
 include_once 'user.class.php'; //import /classes/user.class.php
-include_once '../db_config.php';
+include_once 'db_config.php';
+include_once 'db_connection.php';
 class Student extends User { //create Student class
+
+
+    public function createPost($id,$question,$postDate,$postTime)
+    {
+       // $sql="SELECT * FROM post WHERE users_id='$id'";
+            
+        $sql="INSERT INTO post(users_id,content,upvote,date,time) 
+                                    VALUES('$id','$question',0,'$postDate','$postTime')";
+
+        mysqli_query($this->db, $sql);
+    
+        echo "<script type='text/javascript'>alert('Question has been posted successfully');</script>;";
+
+    }
 
 }
 ?>
