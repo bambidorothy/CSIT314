78d8a06ab387407d52a7502a9d849839ef9f31c9
bambidorothy <bambideleon@gmail.com>
2020-05-19 03:11:09 +0800

    added missing ID column in display table

--- student.php
+++ student.php
@@ -217,6 +217,7 @@ if(isset($_POST["searchButton"])){
   <h1>Your Posts</h1>
   <table class="table table-responsive">
   <tr>
+    <th>ID</th>
     <th>Content</th>
     <th>Upvotes</th>
     <th>Date</th>
@@ -229,6 +230,7 @@ if(isset($_POST["searchButton"])){
   <h1>Public Posts</h1>
   <table class="table table-responsive">
   <tr>
+    <th>ID</th>
     <th>Content</th>
     <th>Upvotes</th>
     <th>Date</th>
